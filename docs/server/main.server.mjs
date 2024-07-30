eccc14f58c52694dc676fb27fbbbdba2f4b4e5bf
Dalia-Fouad <dodda246.df@gmail.com>
2024-07-30 10:46:55 +0300

    first commit

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-NHZACSRL.mjs";import"./chunk-VQM2QM6R.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-TY2OYWWW.mjs";import"./chunk-VQM2QM6R.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -22,10 +22,10 @@ export class HomeComponent {
   proName:string="";
   products:IProduct[]=
   [
-    {name:"Samsung",price:1000,onSale:true,imageUrl:"./assets/images/tv.jpeg"},
-    {name:"HTC",price:2000,onSale:false,imageUrl:"./assets/images/screen.jpg"},
-    {name:"DELL",price:3000,onSale:true,imageUrl:"./assets/images/laptop.webp"},
-    {name:"HP",price:3000,onSale:false,imageUrl:"./assets/images/mobile.avif"}
+    {name:"Samsung",price:1000,onSale:true,imageUrl:"assets/images/tv.jpeg"},
+    {name:"HTC",price:2000,onSale:false,imageUrl:"assets/images/screen.jpg"},
+    {name:"DELL",price:3000,onSale:true,imageUrl:"assets/images/laptop.webp"},
+    {name:"HP",price:3000,onSale:false,imageUrl:"assets/images/mobile.avif"}
   ]
 
 }
